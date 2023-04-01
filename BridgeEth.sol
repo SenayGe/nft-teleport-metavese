54271041276d8336a1070a85ee0225dd59fad060
Senay <senaykarisful@gmail.com>
2023-04-01 23:58:43 +0400

    minor fix

--- BridgeEth.sol
+++ BridgeEth.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// import "@maticnetwork//fx-portal/contracts/blob/main/contracts/tunnel/FxBaseRootTunnel.sol";
-
-import "https://github.com/fx-portal/contracts/blob/main/contracts/examples/erc721-transfer/FxERC721RootTunnel.sol";
 
 
-interface IL1ERC721 is IERC721{
+
+// import "https://github.com/fx-portal/contracts/blob/main/contracts/examples/erc721-transfer/FxERC721RootTunnel.sol";
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/fx-portal/contracts/blob/main/contracts/tunnel/FxBaseRootTunnel.sol";
+
+interface IL1ERC721 is ERC721URIStorage{
     function mint (address _to, string calldata _metadata ) external;
-    function transferToken (address _from, address _to, uint256 _tokenId, bytes _data) external;
+    function transferToken (address _from, address _to, uint256 _tokenId) external;
     
 }
 
@@ -16,6 +19,9 @@ contract BridgeEth is FxBaseRootTunnel, IERC721Receiver{
 
     address _checkpointManager = 0x2890bA17EfE978480615e330ecB65333b880928e; // Goerli testnet
     address _fxRoot = 0x3d1d3E34f7fB6D26245E6640E1c50710eFFf15bA; // Goerli testnet
+
+    bytes32 public constant DEPOSIT = keccak256("DEPOSIT");
+    bytes32 public constant MAP_TOKEN = keccak256("MAP_TOKEN");
 
     address public counterpartL2Bridge;
 
@@ -51,10 +57,7 @@ contract BridgeEth is FxBaseRootTunnel, IERC721Receiver{
         counterpartL2Bridge = L2Bridge;
     }
 
-
-
-
-    /**
+    /*
     * @notice deposit an NFT by transfering it to bridge contract -
         to enable porting to L2
     * @param user is the owner-to-be address on L2, could be the 
@@ -76,14 +79,14 @@ contract BridgeEth is FxBaseRootTunnel, IERC721Receiver{
             tokenId
         );
 
-        string memory tokenURI = IL1ERC721(originalToken).tokenURI(tokenId);
-        bytes memory bridgeMessage = abi.encode(DEPOSIT, abi.encode(rootToken, msg.sender, user, tokenId, tokenURI));
+        string memory _tokenURI = IL1ERC721(originalToken).tokenURI(tokenId);
+        bytes memory bridgeMessage = abi.encode(DEPOSIT, abi.encode(originalToken, msg.sender, user, tokenId, _tokenURI));
         _sendMessageToChild(bridgeMessage);
-        emit DepositNFT(originalToken, msg.sender, user, tokenId);
+        emit NFTDeposited(originalToken, msg.sender, user, tokenId);
 
     }
     
-    /**
+    /*
     @notice processes withdraw message sent from L2 bridge contract when a wrapped -
         nft is burned on L2
     */
@@ -97,7 +100,17 @@ contract BridgeEth is FxBaseRootTunnel, IERC721Receiver{
 
         // transfer token from bridge contract back to original owner
         IL1ERC721(L1TokenAddress).safeTransferFrom(address(this), to, tokenId);
-        emit FxWithdrawERC721(rootToken, L2Token, to, tokenId);
+        // emit FxWithdrawERC721(rootToken, L2Token, to, tokenId);
+    }
+
+
+    function onERC721Received(
+        address, /* operator */
+        address, /* from */
+        uint256, /* tokenId */
+        bytes calldata /* data */
+    ) external pure override returns (bytes4) {
+        return this.onERC721Received.selector;
     }
 
 
